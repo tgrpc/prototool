c71107eafd64ddf50b31e8ef2382e13fd3b57397
shiyongbin <shiyongbin@ezbuy.com>
2018-04-13 09:23:30 +0800

    example

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # TGRPC
 
+## 编译 tgrpc/prototool
+
+```
+go get github.com/tgrpc/prototool
+```
+
 ## Example
 
 [example](example/README.md)
--- example/grpc.sh
+++ example/grpc.sh
@@ -3,6 +3,4 @@
 cd .. && ln -s $GOPATH/src vendor
 pwd
 cd example/idl/uber && \
-echo '{"value":"tgrpc-prototool"}' | prototool -H 'cookie:customer=abcdefg;admin_customer=1234567' grpc foo 0.0.0.0:8080 foo.ExcitedService/Exclamation -
-
-# echo '{"primeMembershipType":2}' | prototool -H 'cookie:65_customer=6484B3E883C2AA05,DPS:1ebl2p:dVitY7ngAoufUcTUk0vZ6xoTbkY;area:SG' grpc nadesico 0.0.0.0:14157 nadesico.Prime/CreatePrimeBill -
+echo '{"value":"tgrpc-prototool"}' | prototool -H 'cookie:customer=abcdefg,admin_customer=1234567;region:CH' grpc foo 0.0.0.0:8080 foo.ExcitedService/Exclamation -
